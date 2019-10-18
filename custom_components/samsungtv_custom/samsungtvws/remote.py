4490d94e6df3951296447aca8b20558367c95f75
giefca <matthieu.gaumy@gmail.com>
2019-10-18 18:00:43 +0200

    Update remote.py

--- custom_components/samsungtv_custom/samsungtvws/remote.py
+++ custom_components/samsungtv_custom/samsungtvws/remote.py
@@ -25,6 +25,8 @@ import logging
 import time
 import ssl
 import websocket
+import xml.etree.ElementTree as ET
+import requests
 
 
 class SamsungTVWS():
@@ -40,6 +42,9 @@ class SamsungTVWS():
         self.key_press_delay = key_press_delay
         self.name = name
         self.connection = None
+        
+        self.mute = False
+        self.volume = 0
 
     def __exit__(self, type, value, traceback):
         self.close()
@@ -260,3 +265,48 @@ class SamsungTVWS():
 
     def blue(self):
         self.send_key('KEY_BLUE')
+
+    def SOAPrequest(self, action, arguments, protocole):
+        headers = {'SOAPAction': '"urn:schemas-upnp-org:service:{protocole}:1#{action}"'.format(action=action, protocole=protocole), 'content-type': 'text/xml'}
+        body = """<?xml version="1.0" encoding="utf-8"?>
+                <s:Envelope xmlns:s="http://schemas.xmlsoap.org/soap/envelope/" s:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
+                    <s:Body>
+                    <u:{action} xmlns:u="urn:schemas-upnp-org:service:{protocole}:1">
+                        <InstanceID>0</InstanceID>
+                        {arguments}
+                    </u:{action}>
+                    </s:Body>
+                </s:Envelope>""".format(action=action, arguments=arguments, protocole=protocole)
+        response = None
+        try:
+            response = requests.post("http://{host}:9197/upnp/control/{protocole}1".format(host=self.host, protocole=protocole), data=body, headers=headers, timeout=0.1)
+            response = response.content
+        except:
+            pass
+        return response
+    
+    def get_volume(self):
+        response = self.SOAPrequest('GetVolume', "<Channel>Master</Channel>", 'RenderingControl')
+        if response is not None:
+            volume_xml = response.decode('utf8')
+            tree = ET.fromstring(volume_xml)
+            for elem in tree.iter(tag='CurrentVolume'):
+                self.volume = elem.text
+        return self.volume
+
+    
+    def set_volume(self, volume):
+        self.SOAPrequest('SetVolume', "<Channel>Master</Channel><DesiredVolume>{}</DesiredVolume>".format(volume), 'RenderingControl')
+    
+    def get_mute(self):
+        response = self.SOAPrequest('GetMute', "<Channel>Master</Channel>", 'RenderingControl')
+        if response is not None:
+            # mute_xml = response.decode('utf8')
+            tree = ET.fromstring(response.decode('utf8'))
+            for elem in tree.iter(tag='CurrentMute'):
+                mute = elem.text
+            if (int(mute) == 0):
+                self.mute = False
+            else:
+                self.mute = True
+        return self.mute
